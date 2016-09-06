212ce7b2583fe025b808b08494f7e30473b3bc6a
bakikm <bakri@gmx.net>
2016-09-06 20:28:13 +0200

    change my_fevorite_hobbies to new page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,4 +5,6 @@ class WelcomeController < ApplicationController
   end
   def study_and_work
   end
+  def my_fevorite_hobbies
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'about_me', to: 'welcome#about_me'
   get 'study_and_work', to: 'welcome#study_and_work'
+  get 'my_fevorite_hobbies', to: 'welcome#my_fevorite_hobbies'
   root 'welcome#index'
 
 
